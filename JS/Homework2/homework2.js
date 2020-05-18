ffce8c527fb2c8e5d68ffdb91f7a0574787fff54
radjke <46851162+radjke@users.noreply.github.com>
2020-05-18 17:16:55 +0400

    reMake

--- JS/Homework2/homework2.js
+++ JS/Homework2/homework2.js
@@ -21,7 +21,7 @@ let car3 = {
     horse_power: "123HP",
     year: 2015,
     color: "white",
-    price: 15000
+    price: 1200
 };
 let car4 = {
     manufacturer: "BMW",
@@ -29,22 +29,25 @@ let car4 = {
     horse_power: "123HP",
     year: 2015,
     color: "white",
-    price: 18000
+    price: 16000
 };
 
 console.log("Manufacturer: " + car.manufacturer + "\nColor: " + car.color + "\nModel: " + car.model + "\nYear: " + car.year + "\nHorse Power: " + car.horse_power + "\nPrice: " + car.price + "\n");
 
 let cars = [];
+let carFound = false;
 cars.push(car);
 cars.push(car2);
 cars.push(car3);
 cars.push(car4);
 for (let i = 0; i < cars.length; i++) {
     if (cars[i].price < 15000) {
-        console.log(cars[i].manufacturer + ' ' + cars[i].model)
-    } else {
-        console.log("სამწუხაროდ ამ ფასში მანქანას ვერ შეიძენთ :( ");
+        console.log(cars[i].manufacturer + ' ' + cars[i].model);
+        carFound = true;
     }
+}
+if (!carFound) {
+    console.log("სამწუხაროდ ამ ფასში მანქანას ვერ შეიძენთ :( ");
 }
 
 
